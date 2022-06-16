92609da94437969317bb0c370a69ab6599d6e8f4
Euan Jed Tabamo <79002463+u1tbmo@users.noreply.github.com>
2022-06-16 17:03:56 +0800

    Update Program

--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Variables
+
+/* Integers:
+An integer is a whole number. It cannot be a decimal.
+The range of int is -2,147,483,648 to 2,147,483,647.
+*/
+
+int a = 99; // Declares an integer a, assigns it a value of 99
+
+/* String:
+A string is a sequence of characters. It is used to represent text.
+*/
+
+string b = "Hello World"; // Declares a string b, assigns it a value of "Hello World"
+
+/* Byte:
+A byte is a whole number. It cannot be a decimal.
+The range of byte is 0 to 255.
+*/
+
+byte c = 255; // Declares a byte c, assigns it a value of 255
+
+/* Char:
+A char is a single character. It represents a character as a UTF-16 code unit.
+
+*/
+
+char d = 'a'; // Declares a char d, assigns it a value of 'a'
+
+/* Float:
+A float is a number with a decimal point. It is less precise than a double.
+The range of float is -3.402823E+38 to 3.402823E+38.
+When declaring a float, place an f at the end to indicate that it is a float.
+Otherwise, you are assigning a double to a float variable.
+*/
+
+float e = 3.14f; // Declares a float e, assigns it a value of 3.14
+
+/* Double:
+A double is a number with a decimal point. It is more precise than a float.
+The range of double is -1.7976931348623157E+308 to 1.7976931348623157E+308.
+*/
+
+double f = 3.14; // Declares a double f, assigns it a value of 3.14
+
+/* Decimal:
+A decimal is a number with a decimal point. It is more precise than a float or double.
+The range of decimal is -7.9228162514264337593543950335E+28 to 7.9228162514264337593543950335E+28.
+When declaring a decimal, place an m at the end to indicate that it is a decimal.
+Otherwise, you are assigning a double to a decimal variable.
+*/
+
+decimal g = 3.14m; // Declares a decimal g, assigns it a value of 3.14
+
+// Prints the values of the variables
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
